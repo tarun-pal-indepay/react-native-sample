d8c94d5a860ebbbce544f5701b4ccb44aa40607e
Chinmay Tayade <97028462+chinmayindepay@users.noreply.github.com>
2024-03-04 10:25:10 +0530

    updated files

--- ios/SampleReactNative/ReactPaymentLib.swift
+++ ios/SampleReactNative/ReactPaymentLib.swift
@@ -9,18 +9,28 @@ import paymentlib
 import Foundation
 
 public class ReactPaymentLib {
+  /// <#Description#>
+  /// - Parameters:
+  ///   - clientId: <#clientId description#>
+  ///   - clientKey: <#clientKey description#>
+  ///   - merchantId: <#merchantId description#>
+  ///   - merchantName: <#merchantName description#>
+  ///   - locale: <#locale description#>
+  ///   - appId: <#appId description#>
+  ///   - env: <#env description#>
+  ///   - merchantLogoUrl: <#merchantLogoUrl description#>
     public static func initIndepaySDK(clientId: String, clientKey: String, merchantId: String, merchantName: String, locale: String, appId: String, env: String, merchantLogoUrl: String) {
-        // Your implementation here
+        // Your   implementation here
         print("Initialized Indepay SDK with Client ID: \(clientId)")
-//      PaymentLib().initIndepaySDK(
-//          clientId: "clientId",
-//          clientKey: "clientKey",
-//          merchantId: "merchantId",
-//          merchantName: "Merchant Name",
-//          locale: "en",
-//          appId: "appId",
-//          env: "dev",
-//          merchantLogoUrl: "merchantLogoUrl"
-//      )
+      PaymentLib().initIndepaySDK(
+          clientId: "clientId",
+          clientKey: "clientKey",
+          merchantId: "merchantId",
+          merchantName: "Merchant Name",
+          locale: "en",
+          appId: "appId",
+          env: "dev",
+          merchantLogoUrl: "merchantLogoUrl"
+      )
     }
 }
